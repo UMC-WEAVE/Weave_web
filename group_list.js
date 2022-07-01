06c0554af1ea5fffab997f34c8944956e743007c
Azamman327 <wjsdbdud327@naver.com>
2022-07-01 20:57:34 +0900

    add group_list page

---
+++ group_list.js
@@ -0,0 +1,5 @@
+var $btnValue = "새로운 그룹을\n만들어보세요";
+// const $btnValue = $btn[0].getAttribute('value');
+// $btn[0].val = $btnValue;
+$(".makeGroupBtn").attr("value", $btnValue)
+// console.log($(".makeGroupBtn").attr("value"));
