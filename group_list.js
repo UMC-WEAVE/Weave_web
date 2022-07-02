82167eb0280d3bf38025f8f94cd66dfd2abc2d01
Azamman327 <wjsdbdud327@naver.com>
2022-07-02 22:10:00 +0900

    real real complete

--- group_list.js
+++ group_list.js
@@ -20,10 +20,8 @@ const groups = [
 const $modal = document.querySelector(".modal");
 const $join_modal = document.querySelector(".join_modal");
 const $makeGroupButton = document.querySelector(".makeGroupBtn");
-// const $joinButton = document.querySelector(".joinBtn");
 const $close_btn = document.querySelector('.modal_close');
 const $add_btn = document.querySelector(".add_group");
-// const $codeSendBtn = document.querySelector(".codeSendBtn");
 const $groupListContainer = document.querySelector(".groupListContainer");
 
 const groupItemTemplate = (newData) => {
@@ -75,11 +73,6 @@ $makeGroupButton.addEventListener('click', () => {
     $modal.style.display = 'block';
 })
 
-// $joinButton.addEventListener('click', () => {
-//     $(".overlay").show();
-//     $join_modal.style.display = 'block';
-// })
-
 $close_btn.addEventListener('click', () => {
     $(".overlay").hide();
     $modal.style.display = 'none';
@@ -89,11 +82,6 @@ $add_btn.addEventListener('click', () => {
     $(".overlay").hide();
     $modal.style.display = 'none';
 })
-
-// $codeSendBtn.addEventListener('click', () => {
-//     $(".overlay").hide();
-//     $join_modal.style.display = 'none';
-// })
 
 /***add new group***/
 console.log(groups);
